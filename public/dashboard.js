6e5346817d743471a8f74af32385dbe6fb0d44aa
Ian <iwade0704@gmail.com>
2023-12-17 21:15:44 -0600

    Front-end Test

--- public/dashboard.js
+++ public/dashboard.js
@@ -4,13 +4,15 @@ fetch('/get-token')
     .then(data => {
         const { token, user_id, user_displayname } = data;
         console.log(data);
-        //requestInitialData(token, user_id, user_displayname);
+        requestInitialData(token, user_id, user_displayname);
     })
     .catch(error => console.error('Error fetching access token:', error));
 
-async function requestInitialData(access_token, user_id) {
-    const profile = await fetchProfile(access_token);
-    console.log(profile);
+async function requestInitialData(access_token, user_id, user_displayname) {
+    console.log("\nrequest initial data\n");
+    console.log(user_id, + " / " + user_displayname);
+    //const profile = await fetchProfile(access_token);
+
     const currently_playing = await fetchPlaybackState(access_token);
     console.log(currently_playing);
 }
